de10294d8f32eff34ff87a45ef4d9dfd3b6a6f10
Mirellafnd22 <146957288+Mirellafnd22@users.noreply.github.com>
2024-10-03 07:49:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,19 +6,44 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 const perguntas = [//abre a lista de objetos (itens)
     {//abre o item
         enunciado:"você usa a inteligencia no dia a dia",
-        alternativas:["Sim","Não"]
+        alternativas:[{
+            texto:"Sim",
+            afirmação: "Afirmação da alternativa 1"
+        },
+        {
+            texto:"não",
+            afirmação: "Afirmação da alternativa"   
+        }
+        ]
     },
     {
         enunciado:"Você acha que a IA pode dominar o mundo",
-        alternativas:["Sim","Não"]
+        alternativas:[{
+            texto:"Sim",
+            afirmação: "afirmação da alternativa 2"
+        },
+        {
+            texto:"não",
+            afirmação:"afirmação da alternativa 2"
+        }
+        ]
     },
     {
         enunciado:"Você é a favor da IA em diversos trabalhos",
-        alternativas:["Sim","Não"]
+        alternativas:[{
+            texto:"Sim",
+            afirmação:"Afirmação da alternativa 3"
+        },
+        {
+            txto:"não",
+            afirmação:"Afirmação da alternativa 3"
+        }
+        ]
     }
 ]
 let posicao = 0;
 let perguntaAtual;
+
 
 function mostraPergunta(){
     perguntaAtual = perguntas[posicao];
@@ -28,8 +53,12 @@ function mostraPergunta(){
 function mostraAlternativas(){
     for (const alternativa of perguntaAtual.alternativa){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", function(){
+            posicao++;
+            mostraPergunta();
+        });
         caixaAlternativa.appendChild (botaoAlternativas);
     }
 }
-mostraPergunta9()
+mostraPergunta();
